e0d8f12f27fd125103caee73d99987201ee3855c
tomiwashelby <tomiwatolugbemi@gmail.com>
2025-01-10 18:53:40 +0100

    new improvements

--- components/Home/Hero.vue
+++ components/Home/Hero.vue
@@ -1,16 +1,14 @@
 <template>
-<div class="pt-32 flex justify-between px-24 -mt-24">
-	<div class="mb-auto">
-		<p class="px-6 py-2 bg-white rounded-[10px] w-52 text-center mb-6">Never stop learning</p>
-		<h1 class="text-[48px] leading-tight font-bold mb-6 w-full text-color-pri">Grow up your skills by online courses with Onlearning</h1>
+<div class="lg:flex lg:justify-between lg:items-center px-24 -mt-12 max-h-screen">
+	<div class="">
+		<p class="px-6 py-2 bg-white rounded-[10px] w-[35%] text-center mb-6">Never stop learning</p>
+		<h1 class="text-[47px] leading-tight font-bold mb-6 text-color-pri">Grow up your skills by online courses with Onlearning</h1>
 		<div class="flex items-center">
 			<NuxtLink class="px-5 py-3 bg-color-sec rounded-[9px] text-white hover:bg-color-pri duration-150 cursor-pointer">EXPLORE PATH</NuxtLink>
 			<div class="ml-4 flex items-center">
-				<div class="flex">
-					<NuxtImg src="/img/testimony1.png" width="40"/>
-					<NuxtImg src="/img/testimony2.png" width="40" class="-ml-3"/>
-					<NuxtImg src="/img/testimony3.png" width="40" class="-ml-3"/>
-				</div>
+				<NuxtImg src="/img/testimony1.png" width="40"/>
+				<NuxtImg src="/img/testimony2.png" width="40" class="-ml-3"/>
+				<NuxtImg src="/img/testimony3.png" width="40" class="-ml-3"/>
 				<div class="ml-2">
 					<div class="flex">
 						<NuxtImg src="/svg/star.svg" width="15" height="15" />
@@ -24,9 +22,7 @@
 			</div>
 		</div>
 	</div>
-	<div class="-mt-[108px]">
-		<NuxtImg src="/img/hero.png" width="1000" height="1000" class="ml-4"/>
-	</div>
+	<NuxtImg src="/img/hero.png" width="550" height="550" class=""/>
 </div>
 </template>
 
--- components/Home/Navbar.vue
+++ components/Home/Navbar.vue
@@ -1,15 +1,15 @@
 <template>
-	<nav class="px-24 h-28 flex items-center justify-between text-color-pri mx-auto">
+	<nav class="px-16 h-28 flex items-center justify-between text-color-pri mx-auto">
 		<NuxtLink to="/">
-			<NuxtImg src="/svg/headerlogo.svg" alt="" class="text-center" width="190"/>
+			<NuxtImg src="/svg/headerlogo.svg" alt="" class="text-center w-[90px] h-[90px] md:w-[190px] md:h-[190px]"/>
 		</NuxtLink>
 		<ul class="flex gap-4 sm:gap-8 md:gap-14">
-			<li v-for="(item, i) in menuItems" :key="i" class="text-color-pri font-semibold hover:text-color-sec duration-200">
+			<li v-for="(item, i) in menuItems" :key="i" class="text-color-pri font-semibold hover:text-color-sec duration-200 text-[8px] md:text-base">
 				<NuxtLink :to="item.path">{{ item.title }}</NuxtLink>
 			</li>
 		</ul>
 		<ul class="flex gap-8">
-			<li v-for="(item, i) in cta" :key="i" class="cursor-pointer px-8 font-semibold py-3 rounded-md hover:scale-[0.95] duration-200" :class="[item.bgColor, item.color]">
+			<li v-for="(item, i) in cta" :key="i" class="cursor-pointer px-8 font-semibold py-3 rounded-md hover:scale-[0.95] duration-200 text-[8px] md:text-base" :class="[item.bgColor, item.color]">
 				<NuxtLink>{{item.title}}</NuxtLink>
 			</li>
 		</ul>
